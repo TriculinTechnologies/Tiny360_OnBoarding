4331a14e21912151d3e3ca6f26dcdad84c0e061f
Tejaswini.R <tejaswini.r@triculintech.com>
2017-01-02 17:38:48 +0530

    test

--- app/four.component.ts
+++ app/four.component.ts
@@ -60,7 +60,7 @@ export class FourComponent implements OnInit {
       }else{
         console.log("Valid Email & Pwd");
         this.spanEmailPwd = false;
-        //test change
+        
       }
        });
     }
